1458fbe17d1c103b151c5c34ab35ed80b39970aa
Dhiraj Shahi <151495872+hackerizzzboi@users.noreply.github.com>
2026-01-17 01:58:41 +0545

    Update syllabus.js

--- js/syllabus.js
+++ js/syllabus.js
@@ -7,8 +7,8 @@ function syllabusPage() {
       <li><a href="assets/syllabus/computer-operator.pdf" target="_blank">Computer Operator (5th Level)</a></li>
       <li><a href="assets/syllabus/computer-technician.pdf" target="_blank">Computer Technician</a></li>
       <li><a href="assets/syllabus/sansad.pdf" target="_blank">Sansad</a></li>
-      <li><a href="assets/syllabus/manav-adhikar.pdf" target="_blank">Human Rights</a></li>
-      <li><a href="assets/syllabus/noc.pdf" target="_blank">IT Assistant</a></li>
+      <li><a href="assets/syllabus/manav-adhikar.pdf" target="_blank">मानव अधिकार</a></li>
+      <li><a href="assets/syllabus/noc.pdf" target="_blank">नेपाल आयल निगम</a></li>
     </ul>
   `;
 }
