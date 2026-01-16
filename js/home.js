b735e77cd80099b934d3fba7494eeaa61da1e597
Dhiraj Shahi <151495872+hackerizzzboi@users.noreply.github.com>
2026-01-17 01:31:25 +0545

    Add homePage function to display welcome message

---
+++ js/home.js
@@ -0,0 +1,15 @@
+function homePage() {
+  return `
+    <h3>Welcome to Computer Operator Pro</h3>
+    <p>This platform helps you prepare for Computer Operator (5th Level).</p>
+
+    <ul>
+      <li>✔ MCQ Practice</li>
+      <li>✔ Full Syllabus PDFs</li>
+      <li>✔ Video Courses</li>
+      <li>✔ Premium Content</li>
+    </ul>
+
+    <p><b>Author:</b> Dhiraj Shahi</p>
+  `;
+}
