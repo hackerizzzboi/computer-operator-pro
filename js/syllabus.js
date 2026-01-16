2ae46dcf345b7fa343566b0ea9ee890b576cc4a0
Dhiraj Shahi <151495872+hackerizzzboi@users.noreply.github.com>
2026-01-17 01:38:56 +0545

    Update syllabus.js

--- js/syllabus.js
+++ js/syllabus.js
@@ -1,3 +1,14 @@
 function syllabusPage() {
-  return `<h3>Syllabus</h3><p>PDF syllabus section.</p>`;
+  return `
+    <h3>Syllabus</h3>
+    <p>Download exam-wise syllabus PDFs:</p>
+
+    <ul>
+      <li><a href="assets/syllabus/computer-operator.pdf" target="_blank">Computer Operator (5th Level)</a></li>
+      <li><a href="assets/syllabus/computer-technician.pdf" target="_blank">Computer Technician</a></li>
+      <li><a href="assets/syllabus/sansad.pdf" target="_blank">Sansad</a></li>
+      <li><a href="assets/syllabus/manav-adhikar.pdf" target="_blank">Human Rights</a></li>
+      <li><a href="assets/syllabus/noc.pdf" target="_blank">IT Assistant</a></li>
+    </ul>
+  `;
 }
