ac323632e0e17171a78e758aa00cff32a4d5c550
Dhiraj Shahi <151495872+hackerizzzboi@users.noreply.github.com>
2026-01-17 01:44:59 +0545

    Update videos.js

--- js/videos.js
+++ js/videos.js
@@ -2,12 +2,10 @@ function videosPage() {
   return `
     <h3>YouTube Video Tutorials</h3>
 
-    <div class="tab-buttons">
-      <button onclick="showTeacher('deepak')">Deepak Yadav (GK)</button>
-      <button onclick="showTeacher('sudip')">Sudip Nepali (GK)</button>
-    </div>
+    <button onclick="showTeacher('deepak')">Deepak Yadav (GK)</button>
+    <button onclick="showTeacher('sudip')">Sudip Nepali (GK)</button>
 
-    <div id="videoContent" class="video-grid">
+    <div id="videoContent" style="margin-top:20px;">
       <p>Select a teacher.</p>
     </div>
   `;
@@ -16,49 +14,20 @@ function videosPage() {
 function showTeacher(name) {
   if (name === "deepak") {
     document.getElementById("videoContent").innerHTML = `
-      <h4>📘 Deepak Yadav – GK</h4>
+      <h4>Deepak Yadav – GK</h4>
       <p>Videos will be added soon.</p>
     `;
   }
 
   if (name === "sudip") {
     document.getElementById("videoContent").innerHTML = `
-      <h4>📕 Sudip Nepali – GK</h4>
+      <h4>Sudip Nepali – GK</h4>
 
-      <h5>GK Series</h5>
-      ${gkVideos().join("")}
+      <p><b>GK Day 1</b></p>
+      <iframe src="https://www.youtube.com/embed/cxgy_Jcw3w8" allowfullscreen></iframe>
 
-      <h5>Public Management</h5>
-      ${pmVideos().join("")}
+      <p><b>GK Day 2</b></p>
+      <iframe src="https://www.youtube.com/embed/QobhKQi89bU" allowfullscreen></iframe>
     `;
   }
-}
-
-function gkVideos() {
-  const ids = [
-    "cxgy_Jcw3w8","QobhKQi89bU","jqQtXmf44CQ","JFWt_iVTnpw","IfC9Keo4fDo",
-    "2EGV3xEZGSk","2JjpdLUaBEs","T2ewQFGg9hU","aI3OYRjpnN0","8o_03Oy07zI",
-    "5cgq_RTqCZ4","6y_tgDm6ulg","7NfO7p8DKR8","pFhXzJOKZi8","uhN6Rl6b6js"
-  ];
-
-  return ids.map((id, i) => `
-    <div class="video-card">
-      <p><b>GK Day ${i + 1}</b></p>
-      <iframe src="https://www.youtube.com/embed/${id}" allowfullscreen></iframe>
-    </div>
-  `);
-}
-
-function pmVideos() {
-  const ids = [
-    "Htz9GZdYvg0","y59joEoiFSY","fvvxxlBnDH0","Xppa2cNtc0M","pz9ofsb2qUU",
-    "bonbURLkDKk","34wJlYIKFfc","z8IlByJMImA","JylqgMVmQSk"
-  ];
-
-  return ids.map((id, i) => `
-    <div class="video-card">
-      <p><b>PM Day ${i + 1}</b></p>
-      <iframe src="https://www.youtube.com/embed/${id}" allowfullscreen></iframe>
-    </div>
-  `);
 }
