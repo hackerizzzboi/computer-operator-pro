503b7f7642c6ef19849b58bfdf87c1232032c625
Dhiraj Shahi <151495872+hackerizzzboi@users.noreply.github.com>
2026-01-17 18:18:33 +0545

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,14 +1,28 @@
 function homePage() {
   return `
-    <div class="card">
-      <h2>👋 Welcome Back!</h2>
-      <p>Prepare smartly for Computer Operator (5th Level).</p>
-      <ul>
-        <li>✅ MCQ Practice</li>
-        <li>📚 Full Syllabus</li>
-        <li>🎥 Video Lectures</li>
-        <li>🔐 Premium Notes</li>
-      </ul>
+
+  <div class="card">
+    <h2>👋 Welcome to Computer Operator Portal</h2>
+
+    <p>
+      Prepare smartly for Computer Operator 5th Level Examination
+    </p>
+  </div>
+
+  <div class="card">
+    <h3>🇳🇵 Nepali Date & Time</h3>
+    <div id="nepaliClock" style="font-size:18px;font-weight:bold;">
+      Loading time...
     </div>
+  </div>
+
+  <div class="card">
+    <h3>📰 Latest Nepal News</h3>
+
+    <div id="newsContainer">
+      Loading news...
+    </div>
+  </div>
+
   `;
 }
